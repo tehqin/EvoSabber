a3c00e11355f47b2a15f5613ee874cea87504da5
Matt Fontaine <tehqin@gmail.com>
2019-06-18 01:17:49 -0400

    Added a test bed for teh new strategy search.

---
+++ TestBed/StratSearch/startSearch.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+#
+# Usage: gputest.sh
+# Change job name and email address as needed 
+#        
+
+# -- our name ---
+#$ -N MapElites
+#$ -S /bin/sh
+# Make sure that the .e and .o file arrive in the
+#working directory
+#$ -cwd
+#Merge the standard out and standard error to one file
+#$ -j y
+# Send mail at submission and completion of script
+# Specify GPU queue
+#$ -q medium
+#$ -l mem_free=14.0G
+/bin/echo Running on host: `hostname`.
+/bin/echo In directory: `pwd`
+/bin/echo Starting on: `date`
+
+# Load CUDA module
+module load mono
+#Full path to executable
+
+mono ~/EvoSabber/MapSabber/MapSabber/bin/Release/MapSabber.exe config/fixedPaladin.tml
